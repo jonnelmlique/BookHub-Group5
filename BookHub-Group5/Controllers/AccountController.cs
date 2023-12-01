392ab5ff95f39e4e260dac00857af2121fc2cb4c
Jonnel Lique <jonnelmlique57@gmail.com>
2023-12-01 21:56:59 +0800

    add google authentication

--- BookHub-Group5/Controllers/AccountController.cs
+++ BookHub-Group5/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace BookHub_Group5.Controllers
 {
@@ -7,6 +10,62 @@ namespace BookHub_Group5.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        public async Task Account()
+        {
+            await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme,
+                new AuthenticationProperties
+                {
+                    RedirectUri = Url.Action("GoogleResponse")
+                });
+        }
+        //public async Task<IActionResult> GoogleResponse()
+        //{
+        //    var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        //    var claims = result.Principal.Identities.FirstOrDefault().Claims.Select(claim => new
+        //    {
+        //        claim.Issuer,
+        //        claim.OriginalIssuer,
+        //        claim.Type,
+        //        claim.Value
+
+        //    });
+
+        //    //return Json(claims);
+
+        //    return RedirectToAction("Library", "User", new {area=""});
+        //}
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var userEmailClaim = result.Principal.Identities.FirstOrDefault()?.Claims
+                .FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+
+            if (userEmailClaim != null)
+            {
+                if (userEmailClaim.Equals("bookhubofficialcontact@gmail.com", StringComparison.OrdinalIgnoreCase))
+                {
+                    return RedirectToAction("Dashboard", "Admin", new { area = "" });
+                }
+                else
+                {
+                    return RedirectToAction("Library", "User", new { area = "" });
+                }
+            }
+
+            return RedirectToAction("Error", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            string googleLogoutUrl = "https://www.google.com/accounts/Logout?continue=https://accounts.google.com/Logout";
+
+            return Redirect(googleLogoutUrl);
+            //await HttpContext.SignOutAsync();
+            //return View("Login");
         }
     }
 }
--- BookHub-Group5/Program.cs
+++ BookHub-Group5/Program.cs
@@ -1,4 +1,21 @@
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+
+})
+    .AddCookie()
+    .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+    {
+        options.ClientId = builder.Configuration.GetSection("GoogleKeys:ClientId").Value;
+        options.ClientSecret = builder.Configuration.GetSection("GoogleKeys:ClientSecret").Value;
+    });
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
