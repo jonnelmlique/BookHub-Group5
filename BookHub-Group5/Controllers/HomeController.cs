9063883eba4858beed79f6c71cf087df08ccbd62
Mark Gervic Arca <markgervicarca@gmail.com>
2023-12-08 15:28:52 +0800

    add footer layout & shop layout

--- BookHub-Group5/Controllers/HomeController.cs
+++ BookHub-Group5/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace BookHub_Group5.Controllers
         {
             return View();
         }
+        public IActionResult Shop()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
